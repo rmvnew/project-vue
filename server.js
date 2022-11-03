adb4f24c53e3767f3aed193750d2efbf60309bda
Ricardo <rmvnew@gmail.com>
2022-11-03 11:11:29 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname+'/dist/'))
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/dist/index.html')
+})
+
+app.listen(4000,function(){
+    console.log('Servidor rodando');
+})
